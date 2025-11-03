cb3a9b86d2f67ffa692213e93cab0c84d3f8da63
urielaquilesmatos-tech <urielaquilesmatos@gmail.com>
2025-11-03 12:51:03 -0300

    Tamplate 2.0

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Rota da Página inicial
 @app.route('/')
 def hello_world():
-    return  render_template("_base.html")
+    return  render_template("home.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
